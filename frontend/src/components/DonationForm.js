d73534ffa5d65d774a72df32f821841ed4d2e43f
Dan Ganiev <dan@danganiev.me>
2021-06-22 13:01:17 +0300

    Frontend folder

---
+++ frontend/src/components/DonationForm.js
@@ -0,0 +1,11 @@
+export default {
+  name: "DonationForm",
+  props: {
+    msg: String,
+  },
+  methods:{
+    donate: function(){
+      alert(1)
+    }
+  }
+};
---
+++ frontend/src/components/DonationForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="donation-form-container">
+    <form class="donation-form">
+      <div>
+        <label for="donation-input">$</label>
+        <input name="donation-input" />
+      </div>
+      <div>
+        <button @click="donate">Donate</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script src="./DonationForm.js"></script>
+<style src="./DonationForm.css"></style>
