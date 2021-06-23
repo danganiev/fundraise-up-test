c0d45d1e47a1764671a5c9925c73bccc2f08a14b
Dan Ganiev <dan@danganiev.me>
2021-06-23 15:44:07 +0300

    donate button & input css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,8 +15,17 @@ export default {
 </script>
 
 <style>
+@font-face {
+  font-family: IBMPlexSans;
+  src: url(./assets/ibm-plex-sans-600.woff2);
+}
+
+body {
+  background-color: #f5f5f5;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: IBMPlexSans, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- frontend/src/components/DonationForm.vue
+++ frontend/src/components/DonationForm.vue
@@ -1,12 +1,73 @@
 <template>
   <div class="donation-form-container">
     <form class="donation-form">
-      <div>
-        <label for="donation-input">$</label>
-        <input name="donation-input" v-model="donation" />
+      <div class="group-price-control">
+        <div class="d-table w-100 p-rel z-index-2">
+          <label for="v-1624441413442-amount" class="d-table-cell text-middle"
+            ><div class="price-symbol">
+              <span class="sr-only">Donation Amount</span>
+              <span qa="currency-symbol">₽</span>
+            </div></label
+          >
+          <div class="d-table-cell text-middle w-100">
+            <input
+              autocomplete="off"
+              class="price-control control-price"
+              type="text"
+              pattern="[\d,]*"
+              inputmode="numeric"
+              qa="amount"
+              placeholder=""
+              v-model="donation"
+            />
+          </div>
+          <div class="d-table-cell text-middle p-rel currency-select">
+            <div class="currency-select-label">
+              <div class="d-table">
+                <div class="d-table-cell text-middle pr-1">RUB</div>
+                <div class="d-table-cell text-middle">
+                  <svg
+                    svg-inline=""
+                    focusable="false"
+                    role="presentation"
+                    width="8"
+                    height="4"
+                    viewBox="0 0 8 4"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="d-block"
+                  >
+                    <path d="M8 0H0l4 4 4-4z" fill="#515151"></path>
+                  </svg>
+                </div>
+              </div>
+            </div>
+            <label for="v-1624441413442-currency" class="sr-only"
+              >Donation Currency</label
+            >
+            <select
+              qa="currency-selector"
+              id="v-1624441413442-currency"
+              class="currency-select-control"
+            >
+              <optgroup label="">
+                <option value="USD">USD | US Dollar</option>
+                <option value="RUB">RUB | Russian Ruble</option>
+                <option value="EUR">EUR | Euro</option>
+                <option value="GBP">GBP | British Pound</option>
+              </optgroup>
+            </select>
+            <div
+              class="p-abs top-left bottom-right currency-select-control-focus"
+            ></div>
+          </div>
+        </div>
       </div>
-      <div>
-        <button @click="donate" type="button">Donate</button>
+
+      <div class="button-container">
+        <button @click="donate" type="button" class="donate-button">
+          <div class="button-inner-container">Donate</div>
+        </button>
       </div>
     </form>
   </div>
